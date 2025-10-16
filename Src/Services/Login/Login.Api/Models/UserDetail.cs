ce4b113b2d9ab2a1ec5e6209638247f09b988677
Paul Prabahar <paul.prabahar@aravind.org>
2025-10-16 18:04:09 +0530

    models created and entity added for user login

---
+++ Src/Services/Login/Login.Api/Models/UserDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace Login.Api.Models;
+
+public class UserDetail
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string Role { get; set; } = default!;
+    public DateTimeOffset Created_At { get; set; }
+    public DateTimeOffset Updated_At { get; set; }
+    public bool IsActive { get; set; }
+}
